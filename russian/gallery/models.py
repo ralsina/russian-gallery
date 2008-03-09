b686fb98862d1ccd2b4519016da430740f1eac8c
Roberto Alsina <ralsina@eeesa.(none)>
2008-03-09 16:04:31 -0200

    Editor form using YUI RTE

--- russian/gallery/models.py
+++ russian/gallery/models.py
@@ -7,11 +7,16 @@ class Page(models.Model):
     pass
   def __unicode__(self):
     return self.title
-  name  = models.CharField(max_length=40)
+  name  = models.CharField('Name',max_length=40)
   '''The path where this page appears'''
-  title = models.CharField(max_length=80)
+  title = models.CharField('Title',max_length=80)
   '''Title of the page'''
-  text  = models.TextField(max_length=3000)
+  text  = models.TextField('Text',max_length=3000)
   '''Some HTML that goes in it'''
   date  = models.DateField(auto_now_add=True)
   '''Date of creation'''
+  
+class PageForm(forms.ModelForm):
+  class Meta:
+    model= Page
+
--- russian/gallery/views.py
+++ russian/gallery/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import get_object_or_404
 from mako_django import render_to_response
-from models import Page
+from models import Page,PageForm
 
 def edit_page (request):
   '''Looks for the page that matches the request's 
@@ -10,9 +10,13 @@ def edit_page (request):
   path='/'.join(request.path.split('/')[1:])
   try:
     p=Page.objects.get(name=path)
+    f=PageForm(p)
   except:
     p=None
-  return render_to_response('editor.html',{ 'page': p})
+    f=PageForm()
+  return render_to_response('editor.html',{ 'page': p,
+                                            'form': f,
+                                          })
 
 def view_page (request):
   '''Looks for the page that matches the request's 
