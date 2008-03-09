22b90e2ff84d8ff324530436a7f10048959ab06c
Roberto Alsina <ralsina@eeesa.(none)>
2008-03-09 12:51:00 -0200

    Initial Django project

---
+++ russian/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^russian/', include('russian.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
